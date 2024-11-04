fde9cc80b6b88ff828af4955f3dbe2843bc26066
Ikebunna <Ikebunnastanley@gmail.com>
2024-11-04 15:26:50 +0100

    initial Commit

---
+++ src/assets/Pages/home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../css/home.css";
+
+const home = () => {
+  return (
+    <div>
+      <div>
+        <a href=""></a>
+      </div>
+    </div>
+  );
+};
+
+export default home;
