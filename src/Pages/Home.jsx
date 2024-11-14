f638a64f2ea75023670aa8316479c2de83099c27
Ikebunna <Ikebunnastanley@gmail.com>
2024-11-14 18:03:34 +0100

    committ

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -13,6 +13,12 @@ import vectline1 from "../../res/Vector 64.png";
 import linee from "../../res/Line 2.png";
 import hugeicon from "../../res/hugeicons.png";
 import brandpic from "../../res/image (2).png";
+import devpic from "../../res/image (3).png";
+import graphpic from "../../res/image (4).png";
+import choosepic from "../../res/image 2.png";
+import choosepic1 from "../../res/image (5).png";
+import logoname from "../../res/Frame 4.png";
+import locicon from "../../res/Vector.png";
 
 const Home = () => {
   // document.title = "";
@@ -200,7 +206,7 @@ const Home = () => {
         </div>
       </div>
       <div className="bg-[#F2F2F2] w-[1257px] h-[672px] mt-[860px]">
-        <p className="absolute font-[montserrat] text-[black] text-[40px] font-medium ml-[270px] mt-[100px]">
+        <p className="absolute font-[montserrat] text-[black] text-[38px] font-medium ml-[270px] mt-[100px]">
           Printing
         </p>
         <img
@@ -208,7 +214,7 @@ const Home = () => {
           src={linee}
           alt=""
         />
-        <p className="absolute font-[montserrat] text-[black] text-[40px] font-medium ml-[475px] mt-[100px]">
+        <p className="absolute font-[montserrat] text-[black] text-[38px] font-medium ml-[475px] mt-[100px]">
           Branding
         </p>
         <img
@@ -216,7 +222,7 @@ const Home = () => {
           src={linee}
           alt=""
         />
-        <p className="absolute font-[montserrat] text-[black] text-[40px] font-medium ml-[700px] mt-[100px]">
+        <p className="absolute font-[montserrat] text-[black] text-[38px] font-medium ml-[700px] mt-[100px]">
           Software Solutions
         </p>
         <div>
@@ -225,7 +231,7 @@ const Home = () => {
             src={hugeicon}
             alt=""
           />
-          <p className="absolute ml-[130px] mt-[480px] text-[32px] text-[black] font-[montserrat] font-light">
+          <p className="absolute ml-[130px] mt-[480px] text-[29px] text-[black] font-[montserrat] font-light">
             Branding-Printing
           </p>
         </div>
@@ -235,7 +241,7 @@ const Home = () => {
             src={hugeicon}
             alt=""
           />
-          <p className="absolute ml-[440px] mt-[480px] text-[32px] text-[black] font-[montserrat] font-light">
+          <p className="absolute ml-[440px] mt-[480px] text-[29px] text-[black] font-[montserrat] font-light">
             Web Design & Development
           </p>
         </div>
@@ -245,12 +251,12 @@ const Home = () => {
             src={hugeicon}
             alt=""
           />
-          <p className="absolute ml-[930px] mt-[480px] text-[32px] text-[black] font-[montserrat] font-light">
+          <p className="absolute ml-[930px] mt-[480px] text-[29px] text-[black] font-[montserrat] font-light">
             Graphics Design
           </p>
         </div>
       </div>
-      <div className="bg-[#EDEDED] w-[1257px] h-[2000px]">
+      <div className="bg-[#EDEDED] w-[1257px] h-[1450px]">
         <div>
           <p className="text-[black] text-[40px] font-[montserrat] font-bold pt-[70px] ml-[500px]">
             Our{" "}
@@ -264,12 +270,131 @@ const Home = () => {
         </div>
         <div>
           <img
-            className="absolute w-[400px] h-[330px] mt-[90px] ml-[170px]"
+            className="absolute w-[400px] h-[330px] mt-[90px] ml-[140px]"
             src={brandpic}
             alt=""
           />
-          <p className="absolute text-[30.32px] font-semibold ml-[620px] mt-[170px] font-poppins text-[black]">
+          <p className="absolute text-[30.32px] font-semibold ml-[580px] mt-[170px] font-poppins text-[black]">
             Branding
+          </p>
+          <p className="absolute text-[12px] text-[#383B40] ml-[580px] mt-[240px] font-normal">
+            With a strong foundation in branding and printing, I bring brands to
+            life with cohesive <br />
+            and impactful visual identities. <br /> <br /> My approach combines
+            strategic branding—developing logos, color schemes, and <br />
+            messaging—with high-quality print design for materials like business
+            cards, brochures, <br />
+            and signage.
+          </p>
+        </div>
+        <div>
+          <img
+            className="absolute w-[400px] h-[330px] mt-[500px] ml-[640px]"
+            src={devpic}
+            alt=""
+          />
+          <p className="absolute align-[right] text-[28px] font-semibold ml-[270px] mt-[570px] font-poppins text-[black]">
+            Software Development
+          </p>
+          <p className="absolute text-right text-[12px] text-[#383B40] ml-[100px] mt-[640px] font-normal">
+            With expertise in web development and software design, I build
+            responsive, user-friendly <br /> websites and applications that meet
+            both functional and aesthetic needs. <br /> <br />
+            i have over 2 years of experience working in the software
+            development industry, and <br />I am proficient in several server
+            side languages and systems used for software development.
+          </p>
+        </div>
+        <div>
+          <img
+            className="absolute w-[400px] h-[290px] mt-[900px] ml-[170px]"
+            src={graphpic}
+            alt=""
+          />
+          <p className="absolute text-[30.32px] font-semibold ml-[630px] mt-[950px] font-poppins text-[black]">
+            Graphics Design
+          </p>
+          <p className="absolute text-[12px] text-[#383B40] ml-[630px] mt-[1000px] font-normal">
+            With a passion for visual storytelling, I specialize in graphic
+            design that communicates ideas <br />
+            clearly and creatively. My skills span digital and print media,
+            including flyers, business cards, <br /> and layouts. <br />
+            <br /> I focus on creating visually compelling designs that align
+            with brand identities and captivate <br /> audiences. I deliver
+            high-quality graphics that enhance brand appeal and achieve
+            strategic goals.
+          </p>
+        </div>
+      </div>
+      <div>
+        <div>
+          <img
+            className="absolute w-[358px] h-[457px] ml-[90px] mt-[70px]"
+            src={choosepic}
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            className="absolute w-[650px] h-[200px] ml-[510px] mt-[70px]"
+            src={choosepic1}
+            alt=""
+          />
+          <p className="absolute ml-[670px] mt-[290px] font-[montserrat] text-[black] font-semibold text-[33px]">
+            Why Choose Us?
+          </p>
+          <p className="absolute text-justify mt-[360px] ml-[520px] text-[black] text-[18px]">
+            We believe in delivering value for money. Our services are
+            competitively priced <br />
+            without compromising on quality, giving you high-end results that
+            fit within your <br /> budget, whether you’re ordering branded
+            merchandise or developing a complex <br /> software platform.
+          </p>
+        </div>
+      </div>
+      <div className="bg-[#07203D] h-[350px] w-[1257px] mt-[650px]">
+        <div>
+          <img
+            className="absolute w-[320px] h-[38px] ml-[200px] mt-[50px]"
+            src={logoname}
+            alt=""
+          />
+          <p className="absolute font-[montserrat] text-[#FFFFFF] text-[14px] ml-[180px] mt-[125px] font-light">
+            We wholeheartedly believe in integrity, treating our <br />
+            clients, partners, and employees with genuine <br /> respect and
+            fairness in every interaction.
+          </p>
+        </div>
+        <div className="font-[montserrat]">
+          <p className="absolute text-[white] text-[24px] mt-[45px] ml-[680px] font-[montserrat]">
+            Quick links
+          </p>
+          <div className="text-[#FFFFFF] absolute mt-[90px] ml-[680px] font-thin">
+            <p className="leading-8">
+              Home <br />
+              Our Service <br />
+              About Us <br />
+              Contact Us
+            </p>
+          </div>
+        </div>
+        <div>
+          <p className="absolute text-[white] text-[24px] mt-[45px] ml-[900px] font-[montserrat]">
+            Contact Us
+          </p>
+          <img
+            className="absolute ml-[900px] mt-[110px]"
+            src={locicon}
+            alt=""
+          />
+          <p className="absolute text-[#FFFFFF] text-[15px] mt-[110px] ml-[930px] font-[montserrat] font-thin">
+            Abuja, Nigeria
+          </p>
+          <p className="absolute text-[#FFFFFF] text-[15px] mt-[150px] ml-[930px] font-[montserrat] font-thin">
+            +2348166067804
+          </p>
+          <p className="absolute text-[#FFFFFF] text-[15px] mt-[190px] ml-[930px] font-[montserrat] font-thin">
+            hexstandigitalagency@gmail.com
           </p>
         </div>
       </div>
